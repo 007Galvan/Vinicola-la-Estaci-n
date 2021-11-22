e2f3ae467c141c31f2cce3d4774a745da964d66b
Alejandro Orbe <josealexorbech@hotmail.com>
2021-11-22 17:42:20 -0600

    carrito y id en los productos

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,46 @@
 var total = document.getElementById("total").innerHTML
+var content = document.querySelector('.shopping')
+var carrito = []
+var productos = [];
 
 window.addEventListener('load', () => {
-
-    productos=JSON.parse(localStorage.getItem('cart'));
-
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+    productos = JSON.parse(localStorage.getItem('productos'))
+    if (productos == null) alert('No hay productos')
+    else if (carrito == null) alert('agrega productos al carrito')
+    else getcart()
+    console.log(carrito)
+    console.log(productos)
 })
+
+function getcart() {
+    carrito.forEach(data => {
+        var compra = productos.find(productos => productos.id === data.id);
+        content.innerHTML += `
+            <tr>
+            <td><img src="${compra.imagen}" alt="" id="imgcart">${compra.nombre}</td>
+            <td>${compra.detalle}</td>
+            <td>${compra.precio}</td>
+            <td><button onClick="morestock(${compra.id})">+</button> 1 <button onClick="lessstock(${compra.id})">-</button></td>
+            <td><button onCLick="del(${data.id})">borrar</button></td>
+            <td>1000</td>
+        </tr>`
+    })
+}
+
+function morestock(compra) {
+    console.log('more');
+    console.log(compra);
+
+}
+function lessstock(compra) {
+    console.log('less');
+    console.log(compra);
+}
+function del(compra) {
+    var delet = productos.find(productos => productos.id === compra);
+    console.log(typeof delet);
+
+    console.log('producto borrado');
+    console.log(compra);
+}
--- js/productos.js
+++ js/productos.js
@@ -1,28 +1,47 @@
 
-var cuerpo=document.querySelector('.cards')
-var productos=[];
+var cuerpo = document.querySelector('.cards')
+var productos = [];
+function mostrarDatos() {
+    productos.forEach(info => {
 
-function mostrarDatos(){
-	productos.forEach(info =>{
-
-		cuerpo.innerHTML+=`<div class="card">
+        cuerpo.innerHTML += `<div class="card">
         <img class="card-img" src="${info.imagen}" alt="" >
         <p>${info.nombre}</p>
         <p>$${info.precio}</p>
         <p>Stock: ${info.stock}</p>
-        <a class="add-to-cart" href="">Agregar al carrito</a>
+        <button class="add-to-cart" id="buy" onClick="buy(${info.id}) " >Agregar al carrito</button>
     </div>`
-	})
+        console.log(info.id)
+    })
+
 }
 
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
 
-    productos=JSON.parse(localStorage.getItem('productos'))
-    if(productos==null){
+    productos = JSON.parse(localStorage.getItem('productos'))
+    carrito = JSON.parse(localStorage.getItem('carrito'));
+
+    if (productos == null) {
         alert('No hay productos que mostrar')
     }
-    else{
+    else {
         mostrarDatos();
+    }
+
+    if (carrito == null) {
+        carrito = [];
     }
 })
 
+
+// prueba compra carrito
+function buy(producto) {
+    console.log('se agrego el producto ' + producto);
+    var cart = {
+        id: producto
+    }
+
+    carrito.push(cart);
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    
+}
