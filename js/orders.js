b52814d4bf8b1d41e86e1e987ef553ac8527fd40
Spartan2502/AlexOrbe <josealexorbech@hotmail.com>
2021-12-09 01:32:24 -0600

    orders working

--- js/orders.js
+++ js/orders.js
@@ -4,29 +4,28 @@ addEventListener('load', () => {
     var session = JSON.parse(sessionStorage.getItem('logged'));
     var sells = JSON.parse(localStorage.getItem('sells'))
     var products = JSON.parse(localStorage.getItem('products'))
-//     sells.forEach((value, index, array) => {
-// console.log(value); 
-//console.log(sells);
-        var find = sells.find(x => x.user == session)
-        let ids = find.prodcutos.map(x => x.id)
-        for (let i = 0; i < ids.length; i++) {
-            compra = products.filter(products => products.id === ids[i]);
+    var count = sells.filter(user => user.user == session)
+    //
+    count.forEach((value, index, array) => {
+        var date = value.date
+        value.prodcutos.forEach((value, index, array) => {
+            var find = products.find(product => product.id == value.id)
             show.innerHTML += `
-        <tr >
-            <td><img src="${compra[0].image}" alt="${compra[0].name}"></td>
-            <td>${compra[0].name} </td>
-            <td>${find.date}</td>
-            <td>${compra[0].price}</td>
+        <tr>
+            <td><img src="${find.image}" alt="${find.name}"></td>
+            <td>${find.name} </td>
+            <td>${date}</td>
+            <td>${find.price}</td>
         </tr>
         `
-        }
+        })
         show.innerHTML += `
     <tr>
     <td></td>
     <td></td>
     <td></td>
-    <td>Total: ${find.total}</td>
-    </tr>`
-    // })
-
+    <td>Total: ${value.total}</td>
+    </tr>
+    <hr>`
+    })
 })
